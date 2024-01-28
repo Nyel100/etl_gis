a60616c7f5e7387c8e1a74b14ef3c249cc1af519
Nyelsen <nyelsenfernandes2@gmailcom>
2024-01-27 22:10:52 -0300

    geocodificacao

--- etl_gis.py
+++ etl_gis.py
@@ -17,6 +17,19 @@ def ler_excel(caminho_arquivo):
         return None
     
 
+#Função para geocodificação
+def geocodificar_dataframe(df):
+    try:
+        dados_geocodificados = geocode(df['ENDEREÇO'], provider = 'nominatim', timeout = None, user_agent = "my_geocoder") 
+        df['Latitude'] = dados_geocodificados['geometry'].y
+        df['Longitude'] = dados_geocodificados['geometry'].x 
+        gdf = gpd.GeoDataFrame(df, geometry = gpd.points_from_xy(df['Longitude'], df['Latitude']))
+        return gdf 
+    except Exception as e:
+        print(f"Erro ao realizar a geocodificação: {e}")
+        return None
+
+
 #chamando o arquivo excel
 filename = (r"E:\vscode_projects\etl_gis_project\etl_gis\PontosApoio.xlsx")
 apoio_df = ler_excel(filename)
@@ -24,5 +37,15 @@ apoio_df = ler_excel(filename)
 if apoio_df is not None:
     print("Tabela Excel lida com sucesso")
     print(apoio_df.head())
+    
+    apoio_df = geocodificar_dataframe(apoio_df)
+    
+    if apoio_df is not None:
+        print("Dados geocodificados com sucesso: ")
+        print(apoio_df.head())
+        
+    else:
+        print("Falha ao realizar a geocodificação")
+    
 else:
     print("Falha ao ler o arquivo Excel")
